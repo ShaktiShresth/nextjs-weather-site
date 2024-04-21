a067ef6cb311086ff25827438b122e0a8249e51e
Shakti Shrestha <75934459+ShaktiShresth@users.noreply.github.com>
2024-04-21 14:56:21 +0545

    env file

--- app/page.js
+++ app/page.js
@@ -15,6 +15,7 @@ export default function Home() {
   const [error, setError] = useState(null);
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
+  console.log(process.env.NEXT_PUBLIC_WEATHER_KEY);
 
   const fetchWeather = async (ev) => {
     ev.preventDefault();
